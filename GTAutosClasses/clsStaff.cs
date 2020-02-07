c8f240c513d7c9e2002d25d5d3951059e57c5fd9
Gurpreet Singh <P2444523@my365.dmu.ac.uk>
2020-02-07 15:41:26 +0000

    Added testing and class for staff management

---
+++ GTAutosClasses/clsStaff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTAutosClasses
+{
+    public class clsStaff
+    {
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public int OfficeId { get; set; }
+        public int PositionId { get; set; }
+        public int ContactNumber { get; set; }
+        public string StaffAddress { get; set; }
+        public DateTime HireDate { get; set; }
+        public Boolean IsEmployed { get; set; }
+
+    }
+}
---
+++ GTAutosTesting/tstStaff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GTAutosClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GTAutosTesting
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Creates new instance of the Staff class
+            clsStaff staffMember = new clsStaff();
+
+            // Test to validate if the new instance is created
+            Assert.IsNotNull(staffMember);
+        }
+    }
+}
