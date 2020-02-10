8f42b5baa69911611400413764c78a2540bdc0cb
avahora866 <57990743+avahora866@users.noreply.github.com>
2020-02-10 10:24:21 +0000

    Added testing and class for stock management

---
+++ GTAutosTesting/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GTAutosClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GTAutosTesting
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStock Stock = new clsStock();
+            Assert.IsNotNull(Stock);
+        }
+    }
+}
