6f545d19f218aaf224a3276f19df532a9f96542d
grimz5129 <59632166+grimz5129@users.noreply.github.com>
2020-02-10 11:20:18 +0000

    i have made a new branch and added the existing files i made previously
    i have made a new branch and added the existing files i made previously

---
+++ GTAutosTesting/tstOffice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GTAutosClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GTAutosTesting
+{
+    [TestClass]
+    public class tstOffice
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsOffice AnOffice = new clsOffice();
+            Assert.IsNotNull(AnOffice);
+        }
+    }
+}
