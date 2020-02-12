a08cdf32b596ee3aa2a913ef1be3efad5f918b80
avahora866 <57990743+avahora866@users.noreply.github.com>
2020-02-12 12:50:32 +0000

    Added 2 classes and 2 test classes (car and request)

---
+++ GTAutosClasses/clsCar.cs
@@ -0,0 +1,14 @@
+﻿namespace GTAutosClasses
+{
+    public class clsCar
+    {
+        public int NumberPlate { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool Sold { get; set; }
+        public int OfficeCode { get; set; }
+    }
+}
---
+++ GTAutosTesting/tstRequest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using GTAutosClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GTAutosTesting
+{
+    [TestClass]
+    public class tstRequest
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsRequest req = new clsRequest();
+            int TestData = 0;
+            req.RequestId = TestData;
+            Assert.AreEqual(req.RequestId, TestData);
+        }
+        public void SellerNameOk()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "Jhon Barton";
+            req.SellerName = TestData;
+            Assert.AreEqual(req.SellerName, TestData);
+        }
+        public void SellerEmailOk()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "JhoneB@hotmail.com";
+            req.SellerEmail = TestData;
+            Assert.AreEqual(req.SellerEmail, TestData);
+        }
+        public void SellerPhoneNumOk()
+        {
+            clsRequest req = new clsRequest();
+            int TestData = 123456789;
+            req.SellerPhoneNum = TestData;
+            Assert.AreEqual(req.SellerPhoneNum, TestData);
+        }
+        public void SellerAddressOk()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "47 Mount Road";
+            req.SellerAddress = TestData;
+            Assert.AreEqual(req.SellerAddress, TestData);
+        }
+        public void NumberPlateOk()
+        {
+            clsRequest req = new clsRequest();
+            int TestData = 12345678;
+            req.NumberPlate = TestData;
+            Assert.AreEqual(req.NumberPlate, TestData);
+        }
+        public void MakePropertyOK()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "Nissan";
+            req.Make = TestData;
+            Assert.AreEqual(req.Make, TestData);
+        }
+
+        [TestMethod]
+        public void ModelPropertyOK()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "micra";
+            req.Model = TestData;
+            Assert.AreEqual(req.Model, TestData);
+        }
+
+        [TestMethod]
+        public void ColourPropertyOK()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "blue";
+            req.Colour = TestData;
+            Assert.AreEqual(req.Colour, TestData);
+        }
+
+        [TestMethod]
+        public void DescriptionPropertyOK()
+        {
+            clsRequest req = new clsRequest();
+            String TestData = "a four wheeler that is blue and ..";
+            req.Description = TestData;
+            Assert.AreEqual(req.Description, TestData);
+        }
+
+        [TestMethod]
+        public void PricePropertyOK()
+        {
+            clsRequest req = new clsRequest();
+            Double TestData = 10.00;
+            req.Price = TestData;
+            Assert.AreEqual(req.Price, TestData);
+        }
+        public void DateOfRequestOk()
+        {
+            clsRequest req = new clsRequest();
+            DateTime TestData = DateTime.Now.Date;
+            req.DateOfRequest = TestData;
+            Assert.AreEqual(req.DateOfRequest, TestData);
+        }
+        public void RequestOk()
+        {
+            clsRequest req = new clsRequest();
+            Boolean TestData = true;
+            req.Request = TestData;
+            Assert.AreEqual(req.Request, TestData);
+        }
+
+        public void PurchasedOk()
+        {
+            clsRequest req = new clsRequest();
+            Boolean TestData = true;
+            req.Purchased = TestData;
+            Assert.AreEqual(req.Purchased, TestData);
+        }
+
+        public void DateOfPurchasedOk()
+        {
+            clsRequest req = new clsRequest();
+            DateTime TestData = DateTime.Now.Date;
+            req.DateOfPurchased = TestData;
+            Assert.AreEqual(req.DateOfPurchased, TestData);
+        }
+    }
+}
