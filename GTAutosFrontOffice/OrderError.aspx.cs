2f021c33cb0087acc3268a90c16d148aa0772f25
hasan <p2501257@my365.dmu.ac.uk>
2020-02-17 11:32:46 +0000

    Created basic error oupt for the find method.

---
+++ GTAutosFrontOffice/OrderError.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GTAutosClasses;
+
+public partial class OrderError : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //AnOrder.OrderID = Convert.ToInt32(TxtOrderID.Text);
+        //Session["AnOrder"] = AnOrder;
+        Response.Write("This Order does not exist " + "</br>");
+    }
+    protected void btnTryAgain_Click1(object sender, EventArgs e)
+    {
+        Response.Redirect("AnOrder.aspx");
+    }
+}
