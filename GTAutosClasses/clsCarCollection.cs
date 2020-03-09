e9147c0e5fd0e67d030dabfd5f16b1766e540857
avahora866 <57990743+avahora866@users.noreply.github.com>
2020-03-09 11:44:19 +0000

    Halfway through W25 labwork. Stopped at modifying the web form.

---
+++ GTAutosClasses/clsCarCollection.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GTAutosClasses
+{
+    public class ClsCarCollection
+    {
+        //private data member for the list
+        List<ClsCar> mCarList = new List<ClsCar>();
+        //public property for the address list
+        public List<ClsCar> CarList
+        {
+            get
+            {
+                //return the private data
+                return mCarList;
+            }
+            set
+            {
+                //set the private data
+                mCarList = value;
+            }
+        }
+        //public property of count
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return CarList.Count;
+            }
+            set
+            {
+                //we shall worry about this later
+            }
+        }
+        public ClsCar ThisCar { get; set; }
+
+        public ClsCarCollection()
+        {
+            //var for the index
+            Int32 index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for data connection
+            clsDataConnection DB = new clsDataConnection();
+            //execute stored procedure
+            DB.Execute("sproc_tblCar_SelectAll");
+            //get the count f records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (index < RecordCount)
+            {
+                //create a blank car
+                ClsCar car = new ClsCar();
+                //read in the fields form the current records
+                car.NumberPlate = Convert.ToString(DB.DataTable.Rows[0]["NumberPlate"]);
+                car.Model = Convert.ToString(DB.DataTable.Rows[0]["Make"]);
+                car.Model = Convert.ToString(DB.DataTable.Rows[0]["Model"]);
+                car.Colour = Convert.ToString(DB.DataTable.Rows[0]["Colour"]);
+                car.Description = Convert.ToString(DB.DataTable.Rows[0]["Description"]);
+                car.Price= Convert.ToDouble(DB.DataTable.Rows[0]["Price"]);
+                car.Sold = Convert.ToBoolean(DB.DataTable.Rows[0]["Sold"]);
+                car.OfficeCode = Convert.ToInt32(DB.DataTable.Rows[0]["OfficeCode"]);
+                //add teh record ot the private data member
+                mCarList.Add(car);
+                //point to the next record
+                index++;
+            }
+
+
+            ClsCar TestItem = new ClsCar();
+            TestItem.NumberPlate = "1234 ABCD";
+            TestItem.Make = "Nissan";
+            TestItem.Model = "Micra";
+            TestItem.Colour = "Blue";
+            TestItem.Description = "4 wheeler";
+            TestItem.Sold = true;
+            TestItem.Price = 100.00;
+            TestItem.OfficeCode = 1;
+            //add the item to the test list
+            mCarList.Add(TestItem);
+            // re initalise the object for some new data
+            TestItem = new ClsCar();
+            TestItem.NumberPlate = "5678 FGHI";
+            TestItem.Make = "BMW";
+            TestItem.Model = "A5";
+            TestItem.Colour = "Green";
+            TestItem.Description = "5 wheeler";
+            TestItem.Sold = false;
+            TestItem.Price = 1000.00;
+            TestItem.OfficeCode = 2;
+            //add the item to the list
+            mCarList.Add(TestItem);
+
+        }
+        
+    }
+}
---
+++ GTAutosTesting/tstCarCollection.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using GTAutosClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GTAutosTesting
+{
+    [TestClass]
+    public class TstCarCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            ClsCarCollection AllCars = new ClsCarCollection();
+            Assert.IsNotNull(AllCars);
+        }
+
+        [TestMethod]
+        public void CarListOK()
+        {
+            ClsCarCollection AllCars = new ClsCarCollection();
+            //create some test data to asign to the property 
+            //in this cas the data needs to be a list of objects
+            List<ClsCar> TestList = new List<ClsCar>();
+            //add an item to the list 
+            //create the item of test data
+            ClsCar TestItem = new ClsCar();
+            //set its properties
+            TestItem.NumberPlate = "1234 ABCD";
+            TestItem.Make = "Nissan";
+            TestItem.Model = "Micra";
+            TestItem.Colour = "Blue";
+            TestItem.Description = "4 wheeler";
+            TestItem.Sold = true;
+            TestItem.Price = 100.00;
+            TestItem.OfficeCode = 1;
+            //add the item to the list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCars.CarList = TestList;
+            Assert.AreEqual(AllCars.CarList, TestList);
+
+        }
+
+     
+
+        [TestMethod]
+        public void ThisCarPropertyOK()
+        {
+            ClsCarCollection AllCars = new ClsCarCollection();
+            //create some test data to assign to the property 
+            ClsCar TestCar = new ClsCar();
+            //set its properties
+            TestCar.NumberPlate = "1234 ABCD";
+            TestCar.Make = "Nissan";
+            TestCar.Model = "Micra";
+            TestCar.Colour = "Blue";
+            TestCar.Description = "4 wheeler";
+            TestCar.Sold = true;
+            TestCar.Price = 100.00;
+            TestCar.OfficeCode = 1;
+            //assign the data to the property 
+            AllCars.ThisCar = TestCar;
+            Assert.AreEqual(AllCars.ThisCar, TestCar);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+
+        {
+            ClsCarCollection AllCars = new ClsCarCollection();
+            //create some test data to asign to the property 
+            //in this cas the data needs to be a list of objects
+            List<ClsCar> TestList = new List<ClsCar>();
+            //add an item to the list 
+            //create the item of test data
+            ClsCar TestItem = new ClsCar();
+            //set its properties
+            TestItem.NumberPlate = "1234 ABCD";
+            TestItem.Make = "Nissan";
+            TestItem.Model = "Micra";
+            TestItem.Colour = "Blue";
+            TestItem.Description = "4 wheeler";
+            TestItem.Sold = true;
+            TestItem.Price = 100.00;
+            TestItem.OfficeCode = 1;
+            //add the item to the list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllCars.CarList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllCars.Count, TestList.Count);
+
+        }
+
+        
+    }
+}
