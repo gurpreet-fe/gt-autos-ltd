7dc2fe4d7d0cf7d9c06c2b169b31623242f15c0c
Hasan Mussa <P2501257@my365.dmu.ac.uk>
2020-05-07 14:57:03 +0100

    Created additional validation and better user expericance via errors.

--- GTAutosFrontOffice/OrderList.aspx.cs
+++ GTAutosFrontOffice/OrderList.aspx.cs
@@ -14,7 +14,7 @@ public partial class OrderList : System.Web.UI.Page
         {
             DisplayOrders();
         }
-        if (lstOrderList.Items.Count == 0) 
+        if (lstOrderList.Items.Count < 0) 
         {
             lblError.Text = "There are no records, please try adding one";
         }
@@ -52,7 +52,7 @@ public partial class OrderList : System.Web.UI.Page
         }
         else
         {
-            lblError.Text = "pleases select a record to delete from the list";
+            lblError.Text = "Please select a record to delete from the list";
         }
     }
 
@@ -68,18 +68,40 @@ public partial class OrderList : System.Web.UI.Page
         }
         else
         {
-            lblError.Text = "pleases select a record to edit from the list";
+            lblError.Text = "Please select a record to edit from the list";
         }
 
     }
     protected void btnApply_Click(object sender, EventArgs e)
     {
-        ClsOrderCollection Orders = new ClsOrderCollection();
-        Orders.ReportByOrderStatus(txtOrderStatus.Text);
-        lstOrderList.DataSource = Orders.OrderList;
-        lstOrderList.DataValueField = "OrderID";
-        lstOrderList.DataTextField = "allProperties";
-        lstOrderList.DataBind();
+
+        if (txtOrderStatus.Text == "")
+        {
+            lblError.Text = "Please enter an Order status before trying to filter";
+        }
+        else
+        {
+            //lblError.Text = "";
+            ClsOrderCollection Orders = new ClsOrderCollection();
+            Orders.ReportByOrderStatus(txtOrderStatus.Text);
+            lstOrderList.DataSource = Orders.OrderList;
+            lstOrderList.DataValueField = "OrderID";
+            lstOrderList.DataTextField = "allProperties";
+            lstOrderList.DataBind();
+
+            if (lstOrderList.Items.Count > 0)
+            {
+                lblError.Text = "";
+            }
+            else if (lstOrderList.Items.Count == 0) 
+            {
+                lblError.Text = "There are no records with the status " + txtOrderStatus.Text + " please try again";
+                Orders.ReportByOrderStatus("");
+                txtOrderStatus.Text = "";
+                DisplayOrders();
+            }
+            
+        }
     }
 
     protected void btnClear_Click(object sender, EventArgs e)
@@ -88,5 +110,45 @@ public partial class OrderList : System.Web.UI.Page
         Orders.ReportByOrderStatus("");
         txtOrderStatus.Text = "";
         DisplayOrders();
+        lblError.Text = "";
+    }
+
+    protected void btnApply2_Click(object sender, EventArgs e)
+    {
+
+        lblError.Text = "";
+        ClsOrderCollection Orders = new ClsOrderCollection();
+        string orderID = txtOrderID.Text;
+        int OID = 0;
+        bool canC = int.TryParse(orderID, out OID);
+        if (canC == false)
+        {
+            lblError.Text = "Please enter a valid OrderID";
+        }
+        else
+        {
+            Orders.ReportByOrderID(Convert.ToInt32(txtOrderID.Text));
+        }
+
+        lstOrderList.DataSource = Orders.OrderList;
+        lstOrderList.DataValueField = "OrderID";
+        lstOrderList.DataTextField = "allProperties";
+        lstOrderList.DataBind();
+
+        if (lstOrderList.Items.Count <= 0)
+        {
+            lblError.Text = "There are no records with the OrderID: " + txtOrderID.Text;
+        }
+    }
+
+
+
+    protected void btnClear2_Click(object sender, EventArgs e)
+    {
+        ClsOrderCollection Orders = new ClsOrderCollection();
+        txtOrderID.Text = "";
+        DisplayOrders();
+        lblError.Text = "";
     }
 }
+
